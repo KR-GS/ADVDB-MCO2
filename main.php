6ef158871d6962da1bfcb2fd69bc7e2ab67c1bda
Kaci Reenea Santos <107488670+KR-GS@users.noreply.github.com>
2024-11-30 13:55:50 +0800

    Created PHP files
    - Changed HTML to PHP
    - Connected MySQL server to webapp
    - Displayed table on PHP website

--- main.php
+++ main.php
@@ -6,6 +6,7 @@
             <button type="button" id="server1">Server 1</button>
             <button type="button" id="server2">Server 2</button>
         </div>
+        <br>
         <div class = "inputData">
             <form id="input" action="">
                 <label for="gameName">Game: </label>
@@ -13,6 +14,7 @@
                 <input type="submit" value="Submit">
             </form>
         </div>
+        <br>
         <div class = "editData">
             <form id="edit" action="">
                 <label for="gameName">Search Existing Game: </label>
@@ -29,8 +31,21 @@
                 <input type="submit" value="Submit">
             </form>
         </div>
+        <br>
         <div class = "displayData">
-
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Game name</th>
+                    <th>Release Date</th>
+                    <th>Required Age</th>
+                    <th>Price</th>
+                    <th>Discount DLC Count</th>
+                </tr>
+                <?php
+                    include("database.php");
+                ?>
+            </table>
         </div>
     </body>
 </html>
